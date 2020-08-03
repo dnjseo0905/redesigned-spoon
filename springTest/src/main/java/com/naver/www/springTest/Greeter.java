d1e41c8647381f48c65007f01f2517b4eff203d1
soldesk <soldesk@DESKTOP-9FVJH15>
2020-08-03 15:56:00 +0900

    springTest ver1.4

---
+++ springTest/src/main/java/com/naver/www/springTest/Greeter.java
@@ -0,0 +1,16 @@
+package com.naver.www.springTest;
+
+public class Greeter {
+	private String format;
+
+	public String getFormat() {
+		return format;
+	}
+
+	public void setFormat(String format) {
+		this.format = format;
+	}
+	public String greet(String guest) {
+		return String.format(format, guest);
+	}
+}
